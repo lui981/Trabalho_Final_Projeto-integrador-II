2cdd14820f9b6a1051441c7d81527bc766e5c759
aluno <aluno@ubunturails.myguest.virtualbox.org>
2023-05-30 11:00:22 -0300

    Todo o designe Terminado!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-
-end
+        before_action :authenticate_user!
+        layout :layout
+        def layout
+            if current_user != nil
+                @layout ="application"
+            else
+                @layout ="applicationlogin"
+            end
+        end  
+    end
